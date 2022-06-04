de03d44fd459e34090e0ae1fc860b0d3e37b0e50
wu.chunyang <s2288156@163.com>
2022-06-04 18:56:40 +0800

    menu component static page

--- app-ui/src/main.ts
+++ app-ui/src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).use(ElementPlus)
+const app = createApp(App);
+app.use(store).use(router).use(ElementPlus)
   .mount('#app');
+// eslint-disable-next-line
+for (const iconName in ElementPlusIconsVue) {
+  if (Reflect.has(ElementPlusIconsVue, iconName)) {
+    app.component(iconName, ElementPlusIconsVue[iconName]);
+  }
+}
