72c7756c06c6f20620157721207e0c463aa2918b
Wu.Chunyang <extern.Chunyang.Wu@kuka.com>
2022-07-15 09:52:17 +0800

    fix error

--- app-ui/src/main.ts
+++ app-ui/src/main.ts
@@ -1,18 +1,15 @@
 import { createApp } from 'vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
-
 import 'element-plus/dist/index.css'
-
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-
 const app = createApp(App)
 
 // @ts-ignore
-app.use(createPinia()).use(router).use(ElementPlus, { size: 'default' }).mount('#app')
+app.use(createPinia()).use(router)
+  .use(ElementPlus, { size: 'default' }).mount('#app')
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-//1
