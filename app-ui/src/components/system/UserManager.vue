a38a039ea2416e435731047a49f3724c2bc26ad4
Wu.Chunyang <extern.Chunyang.Wu@kuka.com>
2022-07-22 09:59:22 +0800

    pagination

--- app-ui/src/components/system/UserManager.vue
+++ app-ui/src/components/system/UserManager.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-button type="primary" icon="EditPen" class="filter-item" >新增</el-button>
+      <el-button type="primary" icon="EditPen" class="filter-item">新增</el-button>
     </div>
+
+    <el-table :data="tableData.pages" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180"/>
+      <el-table-column prop="name" label="Name" width="180"/>
+      <el-table-column prop="address" label="Address"/>
+    </el-table>
+
+    <el-pagination background layout="prev, pager, next" :page-size="tableData.pageSize" :page-count="tableData.pageCount"/>
   </div>
 </template>
 
 <script lang="ts" setup>
+
+const tableData = {
+  'pageSize': 2,
+  'pageCount': 1,
+  'pages': [
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    }
+  ]
+}
 
 </script>
 
