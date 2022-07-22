58fe8365009b5c3f1edfaf902938a234dc27fb70
Wu.Chunyang <extern.Chunyang.Wu@kuka.com>
2022-07-22 13:10:37 +0800

    init sql

---
+++ application/src/main/resources/db/migration/V2022_1__init_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE t_account
+(
+    id          int8        NOT NULL,
+    create_time timestamp   NOT NULL,
+    update_time timestamp   NOT NULL,
+    "password"  varchar(64) NOT NULL,
+    username    varchar(32) NOT NULL,
+    CONSTRAINT t_account_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE t_role
+(
+    id          int8        NOT NULL,
+    create_time timestamp   NOT NULL,
+    update_time timestamp   NOT NULL,
+    "name"      varchar(64) NOT NULL,
+    CONSTRAINT t_role_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE t_account_role
+(
+    account_id int8 NOT NULL,
+    role_id    int8 NOT NULL,
+    CONSTRAINT t_account_role_pkey PRIMARY KEY (account_id, role_id)
+);
