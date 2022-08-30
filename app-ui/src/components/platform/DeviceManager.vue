86b2107da70466958a81c78e6b8580a8ac343006
JackWu <s2288156@163.com>
2022-08-30 19:40:27 +0800

    add file

---
+++ app-ui/src/components/platform/DeviceManager.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>设备管理</h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ app-ui/src/components/system/UserManager.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-button type="primary" icon="EditPen" class="filter-item">新增</el-button>
+    </div>
+
+    <el-table :data="tableData.pages" style="width: 100%">
+      <el-table-column prop="createTime" label="CreateDate" width="180"/>
+      <el-table-column prop="updateTime" label="UpdateDate" width="180"/>
+      <el-table-column prop="username" label="Username" width="180"/>
+    </el-table>
+
+    <el-pagination background layout="prev, pager, next" :page-size="tableData.size" :page-count="tableData.totalPages"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { getAccounts } from '@/api/account'
+import { PageQuery } from '@/utils/datas'
+
+const pageQuery = new PageQuery(0, 10)
+const tableData = getAccounts(pageQuery)
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
