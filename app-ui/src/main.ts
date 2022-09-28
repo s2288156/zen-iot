f2aa505ccdfe7035e19642a01f96cc9685d6a2ec
JackWu <s2288156@163.com>
2022-09-28 16:36:40 +0800

    eslintrc.js config

--- app-ui/src/main.ts
+++ app-ui/src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-// import '@/style/index.scss'
 import App from './App.vue'
 import router from './router'
 
@@ -13,7 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-// app.use(ElementPlus, { size: 'default' })
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
