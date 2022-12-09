7294a7c3af2045aeb69fea0ca0c3b4b47a1c1857
JackWu <s2288156@163.com>
2022-12-09 17:40:02 +0800

    save simulator api

--- application/src/main/java/org/zeniot/server/controller/SimulatorController.java
+++ application/src/main/java/org/zeniot/server/controller/SimulatorController.java
@@ -1,13 +1,31 @@
 package org.zeniot.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zeniot.api.SimulatorService;
+import org.zeniot.data.base.SingleResponse;
+import org.zeniot.data.domain.simulator.Simulator;
 
 /**
  * @author Wu.Chunyang
  */
 @RequestMapping("/api")
 @RestController
-public class SimulatorController extends AbstractController{
+public class SimulatorController extends AbstractController {
+
+    private final SimulatorService simulatorService;
+
+    public SimulatorController(SimulatorService simulatorService) {
+        this.simulatorService = simulatorService;
+    }
+
+    @PostMapping("/simulator")
+    public SingleResponse<Simulator> saveSimulator(@RequestBody @Validated Simulator simulator) {
+        return SingleResponse.success(simulatorService.saveSimulator(simulator));
+    }
 
 }
