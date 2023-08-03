90138e7940852953b8b0e9e19ecaefcacfa19ad5
JackWu <s2288156@163.com>
2023-08-03 15:36:23 +0800

    logiclow vue try

--- app-ui/src/components/flow/NodePalette.vue
+++ app-ui/src/components/flow/NodePalette.vue
@@ -9,7 +9,7 @@ const props = defineProps({
   }
 });
 
-const startDrag = (item: IBaseModel) => {
+const startDrag = (item) => {
   const { lf } = props;
   lf.dnd.startDrag({
     type: item.type,
@@ -19,8 +19,8 @@ const startDrag = (item: IBaseModel) => {
 
 const baseNodes = ref([
   {
-    type: 'circle',
-    text: '开始',
+    type: 'fetch-node',
+    text: 'fetch',
     label: '开始节点',
     icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAAH6ji2bAAAABGdBTUEAALGPC/xhBQAAAnBJREFUOBGdVL1rU1EcPfdGBddmaZLiEhdx1MHZQXApraCzQ7GKLgoRBxMfcRELuihWKcXFRcEWF8HBf0DdDCKYRZpnl7p0svLe9Zzbd29eQhTbC8nv+9zf130AT63jvooOGS8Vf9Nt5zxba7sXQwODfkWpkbjTQfCGUd9gIp3uuPP8bZ946g56dYQvnBg+b1HB8VIQmMFrazKcKSvFW2dQTxJnJdQ77urmXWOMBCmXM2Rke4S7UAW+/8ywwFoewmBps2tu7mbTdp8VMOkIRAkKfrVawalJTtIliclFbaOBqa0M2xImHeVIfd/nKAfVq/LGnPss5Kh00VEdSzfwnBXPUpmykNss4lUI9C1ga+8PNrBD5YeqRY2Zz8PhjooIbfJXjowvQJBqkmEkVnktWhwu2SM7SMx7Cj0N9IC0oQXRo8xwAGzQms+xrB/nNSUWVveI48ayrFGyC2+E2C+aWrZHXvOuz+CiV6iycWe1Rd1Q6+QUG07nb5SbPrL4426d+9E1axKjY3AoRrlEeSQo2Eu0T6BWAAr6COhTcWjRaYfKG5csnvytvUr/WY4rrPMB53Uo7jZRjXaG6/CFfNMaXEu75nG47X+oepU7PKJvvzGDY1YLSKHJrK7vFUwXKkaxwhCW3u+sDFMVrIju54RYYbFKpALZAo7sB6wcKyyrd+aBMryMT2gPyD6GsQoRFkGHr14TthZni9ck0z+Pnmee460mHXbRAypKNy3nuMdrWgVKj8YVV8E7PSzp1BZ9SJnJAsXdryw/h5ctboUVi4AFiCd+lQaYMw5z3LGTBKjLQOeUF35k89f58Vv/tGh+l+PE/wG0rgfIUbZK5AAAAABJRU5ErkJggg=='
   }
@@ -37,7 +37,10 @@ const baseNodes = ref([
   <div class="node-collapse">
     <el-collapse>
       <el-collapse-item title="基础节点">
-        <div v-for="(item, index) in baseNodes" :key="index">
+        <div
+          v-for="(item, index) in baseNodes"
+          @mousedown="startDrag(item)"
+          :key="index">
           <div>{{ item.text }}</div>
           <div :style="{ backgroundImage: `url(${item.icon})`}"></div>
         </div>
