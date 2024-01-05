ca9235aac0ff5d8f8a2acecfd5ed0238fd19a010
JackWu <s2288156@163.com>
2024-01-05 18:05:27 +0800

    feat(EtherNet/IP): EtherNetIPClient.readTag test three tag read

--- common/ethernet-ip/src/main/java/org/zeniot/transport/EtherNetTest.java
+++ common/ethernet-ip/src/main/java/org/zeniot/transport/EtherNetTest.java
@@ -1,27 +1,30 @@
 package org.zeniot.transport;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.zeniot.transport.etherip.EtherNetIPClient;
 import org.zeniot.transport.etherip.nio.protocol.CIPData;
 
 /**
  * @author Jack Wu
  */
+@Slf4j
 public class EtherNetTest {
-    private static final String BTN_START = "Btn_Start";
-    private static final String BTN_STOP = "Btn_Stop";
-    private static final String MOTOR_CTRL = "MotorCtrl";
+    private static final String TAG_BTN_START = "Btn_Start";
+    private static final String TAG_BTN_STOP = "Btn_Stop";
+    private static final String TAG_MOTOR_CTRL = "MotorCtrl";
 
     private static String address = "172.22.252.207";
     private static int slot = 2;
     private static short array = 1;
-    private static String tag = BTN_STOP;
     private static CIPData write = null;
     @SneakyThrows
     public static void main(String[] args) {
-        try (EtherNetIPClient client = new EtherNetIPClient("172.22.252.207", 2)) {
+        try (EtherNetIPClient client = new EtherNetIPClient(address, slot)) {
             client.connectTcp();
-            System.out.println(client.readTag(MOTOR_CTRL, array));
+            log.info("{}:{}", TAG_BTN_START, client.readTag(TAG_BTN_START, array));
+            log.info("{}:{}", TAG_BTN_STOP, client.readTag(TAG_BTN_STOP, array));
+            log.info("{}:{}", TAG_MOTOR_CTRL, client.readTag(TAG_MOTOR_CTRL, array));
         }
     }
 }
