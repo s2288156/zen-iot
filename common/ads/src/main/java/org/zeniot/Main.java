85e175711155a9f6878095b8bda7c1f262c3e43e
JackWu <s2288156@163.com>
2024-05-16 14:31:02 +0800

    feat(transport): init ads protocol module

--- common/ads/src/main/java/org/zeniot/Main.java
+++ common/ads/src/main/java/org/zeniot/Main.java
@@ -1,10 +1,26 @@
 package org.zeniot;
 
+import org.apache.plc4x.java.api.PlcConnection;
+import org.apache.plc4x.java.api.PlcDriverManager;
+import org.apache.plc4x.java.api.messages.PlcReadRequest;
+
 /**
  * @author Wu.Chunyang
  */
 public class Main {
     public static void main(String[] args) {
+        String connectionString = "ads://127.0.0.1";
+
+        try (PlcConnection plcConnection = PlcDriverManager.getDefault().getConnectionManager().getConnection(connectionString)) {
+            if (plcConnection.getMetadata().isReadSupported()) {
+                PlcReadRequest.Builder builder = plcConnection.readRequestBuilder();
+                // AdsReadRequest readRequest = new AdsReadRequest();
+            } else if (plcConnection.getMetadata().isWriteSupported()) {
+
+            }
+        } catch (Exception e) {
+
+        }
         System.out.println("Hello world!");
     }
 }
