4077ebbc8da6e10d30dd71fd4c5a717685ffdb0b
JackWu <s2288156@163.com>
2024-07-18 10:43:12 +0800

    feat(ads): coding

--- common/ads/src/test/java/org/zeniot/ads/AdsClientTest.java
+++ common/ads/src/test/java/org/zeniot/ads/AdsClientTest.java
@@ -15,13 +15,13 @@ class AdsClientTest {
 
     @Test
     void write() {
-        // String ip = "47.113.227.247";
-        String ip = "172.29.130.198";
+        String ip = "10.86.201.153";
+        // String ip = "172.29.130.198";
 
-        // String sourceAmsNetId = "172.24.143.55.1.1";
-        String sourceAmsNetId = "172.29.128.1.1.1";
+        String sourceAmsNetId = "10.86.195.33.1.1";
+        // String sourceAmsNetId = "172.29.128.1.1.1";
         int sourceAmsPort = 851;
-        String targetAmsNetId = "172.28.131.49.1.1";
+        String targetAmsNetId = "192.168.56.1.1.1";
         int targetAmsPort = 851;
         AdsClient adsClient = new AdsClient(ip, 48898, targetAmsNetId, targetAmsPort, sourceAmsNetId, sourceAmsPort);
         // 如果使用属性名称，需要通过一个命令查询到名称和indexOffset的对应关系，最终报文中发送的是indexOffset
